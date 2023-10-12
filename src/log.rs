f31eb8b2b1b402daf1ef0fcfc55ec781c4f7a170
qiujiangkun <qjk2001@gmail.com>
2023-10-12 17:56:17 +0900

    [dev] Add 'Detail' log level and adjust filter logic
    Introduced a new log level, 'Detail', into the LogLevel enum for finer-grained control. Reordered enum for logical hierarchy. Altered build_env_filter function to adjust log directives based on the presence of 'Detail' log level to provide comprehensive debugging information when necessary.

--- src/log.rs
+++ src/log.rs
@@ -5,16 +5,17 @@ use tracing::level_filters::LevelFilter;
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt, EnvFilter};
 
-#[derive(Default, Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "lowercase")]
 pub enum LogLevel {
     #[default]
     Off,
-    Trace,
-    Debug,
-    Info,
-    Warn,
     Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+    Detail,
 }
 impl LogLevel {
     pub fn as_level_filter(&self) -> LevelFilter {
@@ -25,6 +26,7 @@ impl LogLevel {
             LogLevel::Debug => LevelFilter::DEBUG,
             LogLevel::Trace => LevelFilter::TRACE,
             LogLevel::Off => LevelFilter::OFF,
+            LogLevel::Detail => LevelFilter::TRACE,
         }
     }
 }
@@ -38,32 +40,33 @@ impl FromStr for LogLevel {
             "info" => Ok(LogLevel::Info),
             "debug" => Ok(LogLevel::Debug),
             "trace" => Ok(LogLevel::Trace),
+            "detail" => Ok(LogLevel::Detail),
             "off" => Ok(LogLevel::Off),
             _ => Err(eyre!("Invalid log level: {}", s)),
         }
     }
 }
 fn build_env_filter(log_level: LogLevel) -> Result<EnvFilter> {
-    let filter = EnvFilter::from_default_env()
-        .add_directive(log_level.as_level_filter().into())
-        .add_directive("tungstenite::protocol=debug".parse()?)
-        .add_directive("tokio_postgres::connection=debug".parse()?)
-        .add_directive("tokio_util::codec::framed_impl=debug".parse()?)
-        .add_directive("tokio_tungstenite=debug".parse()?)
-        .add_directive("h2=info".parse()?)
-        .add_directive("rustls::client::hs=info".parse()?)
-        .add_directive("rustls::client::tls13=info".parse()?)
-        .add_directive("hyper::client=info".parse()?)
-        .add_directive("hyper::proto=info".parse()?)
-        .add_directive("mio=info".parse()?)
-        .add_directive("want=info".parse()?);
+    let mut filter =
+        EnvFilter::from_default_env().add_directive(log_level.as_level_filter().into());
+    if log_level != LogLevel::Detail {
+        filter = filter
+            .add_directive("tungstenite::protocol=debug".parse()?)
+            .add_directive("tokio_postgres::connection=debug".parse()?)
+            .add_directive("tokio_util::codec::framed_impl=debug".parse()?)
+            .add_directive("tokio_tungstenite=debug".parse()?)
+            .add_directive("h2=info".parse()?)
+            .add_directive("rustls::client::hs=info".parse()?)
+            .add_directive("rustls::client::tls13=info".parse()?)
+            .add_directive("hyper::client=info".parse()?)
+            .add_directive("hyper::proto=info".parse()?)
+            .add_directive("mio=info".parse()?)
+            .add_directive("want=info".parse()?);
+    }
     Ok(filter)
 }
 pub fn setup_logs(log_level: LogLevel) -> Result<()> {
     LogTracer::init().context("Cannot setup_logs")?;
-    color_eyre::config::HookBuilder::new()
-        .panic_section("ERROR|FATAL program panicked")
-        .install()?;
     let filter = build_env_filter(log_level)?;
 
     let subscriber = fmt()
