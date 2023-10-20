cc0222a914d323bb2d433a0c88e3d3ebf1bd17d0
qiujiangkun <qjk2001@gmail.com>
2023-10-20 17:36:12 +0900

    [dev] Refactor service_name parameter to use references

--- src/config.rs
+++ src/config.rs
@@ -38,7 +38,7 @@ pub struct ServerConfig {
     pub debug: bool,
 }
 
-pub fn load_config<Config: DeserializeOwned + Debug>(mut service_name: String) -> Result<Config> {
+pub fn load_config<Config: DeserializeOwned + Debug>(service_name: impl AsRef<str>) -> Result<Config> {
     let args: CliArgument = CliArgument::parse();
 
     println!("Working directory {}", current_dir()?.display());
@@ -46,12 +46,13 @@ pub fn load_config<Config: DeserializeOwned + Debug>(mut service_name: String) -
     let config = std::fs::read_to_string(&args.config)?;
     let config: Value = serde_json::from_str(&config)?;
     if let Some(entry) = args.config_entry {
-        service_name = entry;
+        parse_config(config, &entry)
+    } else {
+        parse_config(config, service_name.as_ref())
     }
-    parse_config(config, service_name)
 }
 
-pub fn parse_config<Config: DeserializeOwned + Debug>(mut config: Value, service_name: String) -> Result<Config> {
+pub fn parse_config<Config: DeserializeOwned + Debug>(mut config: Value, service_name: &str) -> Result<Config> {
     let service_config = config
         .get_mut(&service_name)
         .ok_or_else(|| eyre!("Service {} not found in config", service_name))?
@@ -60,10 +61,10 @@ pub fn parse_config<Config: DeserializeOwned + Debug>(mut config: Value, service
     for (k, v) in service_config.as_object().unwrap() {
         root.insert(k.clone(), v.clone());
     }
-    if service_config.get(&service_name).is_none() {
-        root.remove(&service_name);
+    if service_config.get(service_name).is_none() {
+        root.remove(service_name);
     }
-    root.insert("name".to_string(), Value::String(service_name.clone()));
+    root.insert("name".to_string(), Value::String(service_name.to_string()));
     let config: Config = serde_json::from_value(config)?;
     println!("App config {:#?}", config);
     Ok(config)
