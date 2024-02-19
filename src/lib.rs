0d37ffe17c61d5998dbba9b518db16833eb61ca5
JakkuSakura <qjk2001@gmail.com>
2024-02-20 01:22:00 +0800

    [dev] cleanup common-rs re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,7 @@
 // these are re-exports from other crates
 
-pub use async_compat::{Compat, CompatExt};
-pub use async_trait::async_trait;
 pub use bytes::{self, Buf, BufMut, Bytes, BytesMut};
-pub use chrono::{
-    DateTime, Datelike, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Timelike, Utc,
-};
-pub use derivative::{self, Derivative};
 pub use eyre::{bail, ensure, eyre, Context as EyreContext, ContextCompat, Error, Result};
-pub use futures::{
-    future::{
-        join_all, lazy, poll_fn as run_fn, poll_immediate as poll_fut, try_join_all, BoxFuture,
-        LocalBoxFuture,
-    },
-    join, pending, pin_mut, poll as poll_once, ready,
-    stream::{BoxStream, FuturesOrdered, FuturesUnordered, LocalBoxStream},
-    task::{noop_waker, noop_waker_ref, Context as FutureContext, Poll},
-    try_join, AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt,
-    Future, FutureExt, Sink, SinkExt, Stream, StreamExt,
-};
-pub use minimal_executor::{block_fn, block_on, poll_fn, poll_on};
-pub use serde::{de::DeserializeOwned, Deserialize, Serialize};
-pub use serde_json::{self, Value};
-pub use static_assertions;
-pub use static_assertions::*;
-pub use std::{
-    any::{type_name, Any},
-    cell::OnceCell,
-    collections::{BTreeMap, BTreeSet, HashMap, HashSet, LinkedList, VecDeque},
-    default::Default,
-    future::{pending, ready},
-    io::{Error as StdIoError, ErrorKind as StdIoErrorKind},
-    marker::{PhantomData, Unpin},
-    ops::{Deref, DerefMut, Fn, FnMut, FnOnce},
-    pin::Pin,
-    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
-};
-pub use thiserror::Error;
-pub use tokio::{
-    self,
-    io::{
-        AsyncBufReadExt as TokioAsyncBufReadExt, AsyncReadExt as TokioAsyncReadExt,
-        AsyncWriteExt as TokioAsyncWriteExt,
-    },
-    task::yield_now,
-};
 pub use tracing::{debug, error, info, instrument as tracing_instrument, span, trace, warn, Level};
 // these are modules of the crate
 
