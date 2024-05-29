39b4e3aa93f3d822010c649b75911be97d591ba7
JakkuSakura <qjk2001@gmail.com>
2024-05-29 11:29:59 +0800

    feat: simplify common-rs

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,12 @@
-use crate::load_env_recursively;
-use clap::Parser;
-use eyre::*;
-use serde::de::DeserializeOwned;
-use serde_json::Value;
 use std::env::current_dir;
 use std::fmt::Debug;
 use std::path::PathBuf;
+
+use clap::Parser;
+use eyre::{bail, Result};
+use serde::de::DeserializeOwned;
+
+use crate::load_env_recursively;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -20,22 +21,9 @@ struct CliArgument {
     value_hint = clap::ValueHint::FilePath
     )]
     config: Option<PathBuf>,
-    /// The path to config file
-    #[clap(long)]
-    config_entry: Option<String>,
 }
 
-pub fn load_config<Config: DeserializeOwned + Debug>(
-    service_name: impl AsRef<str>,
-) -> Result<Config> {
-    load_config_with_default_path(service_name, "etc/config.json")
-}
-
-pub fn load_config_with_default_path<Config: DeserializeOwned + Debug>(
-    service_name: impl AsRef<str>,
-    path: impl AsRef<str>,
-) -> Result<Config> {
-    println!("Loading environment");
+pub fn load_config<Config: DeserializeOwned + Debug>(path: impl AsRef<str>) -> Result<Config> {
     if let Err(err) = load_env_recursively() {
         println!("Failed to load environment: {}", err);
     }
@@ -49,31 +37,20 @@ pub fn load_config_with_default_path<Config: DeserializeOwned + Debug>(
     let config = args.config.unwrap_or_else(|| PathBuf::from(path.as_ref()));
     println!("Loading config from {}", config.display());
     let config = std::fs::read_to_string(&config)?;
-    let config: Value = serde_json::from_str(&config)?;
-    if let Some(entry) = args.config_entry {
-        parse_config(config, &entry)
-    } else {
-        parse_config(config, service_name.as_ref())
-    }
+    parse_config(&config)
 }
 
-pub fn parse_config<Config: DeserializeOwned + Debug>(
-    mut config: Value,
-    service_name: &str,
-) -> Result<Config> {
-    let service_config = config
-        .get_mut(&service_name)
-        .ok_or_else(|| eyre!("Service {} not found in config", service_name))?
-        .clone();
-    let root = config.as_object_mut().unwrap();
-    for (k, v) in service_config.as_object().unwrap() {
-        root.insert(k.clone(), v.clone());
-    }
-    if service_config.get(service_name).is_none() {
-        root.remove(service_name);
-    }
-    root.insert("name".to_string(), Value::String(service_name.to_string()));
-    let config: Config = serde_json::from_value(config)?;
+pub fn parse_config<Config: DeserializeOwned + Debug>(content: &str) -> Result<Config> {
+    let jd = &mut serde_json::Deserializer::from_str(content);
+
+    let result: Result<Config, _> = serde_path_to_error::deserialize(jd);
+    let config = match result {
+        Ok(config) => config,
+        Err(err) => {
+            let path = err.path().to_string();
+            bail!("Failed to parse config at {}: {}", path, err.inner())
+        }
+    };
     println!("App config {:#?}", config);
     Ok(config)
 }
